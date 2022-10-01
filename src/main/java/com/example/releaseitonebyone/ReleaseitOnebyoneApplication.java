99afdacc9530ecd72dfc34f7adb09e0f8300eecd
aaronsleepy <aaron@kmong.com>
2022-10-01 11:35:43 +0900

    first commit

---
+++ src/main/java/com/example/releaseitonebyone/ReleaseitOnebyoneApplication.java
@@ -0,0 +1,13 @@
+package com.example.releaseitonebyone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReleaseitOnebyoneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReleaseitOnebyoneApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/releaseitonebyone/ReleaseitOnebyoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.releaseitonebyone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReleaseitOnebyoneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
